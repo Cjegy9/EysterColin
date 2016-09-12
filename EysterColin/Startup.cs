d774148daca123bf0befb7ad5467f23422d143a5
Colin Eyster <cjegy9@mst.edu>
2016-09-12 11:33:19 -0500

    Add project files.

---
+++ EysterColin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EysterColin.Startup))]
+namespace EysterColin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
